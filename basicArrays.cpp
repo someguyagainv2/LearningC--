924e7007d2b9ff6b4d63d600b70999a93cdaddd9
Malakia Daley <145267660+MalakiaDaley@users.noreply.github.com>
2024-01-31 12:57:30 +0000

    Create basicArrays.cpp

---
+++ basicArrays.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int numbers[5] = {1, 2,3,4,5};
+
+  cout << numbers[0];
+  cout << numbers[1];
+  cout << numbers[2];
+  cout << numbers[3];
+  cout << numbers[4];
+  
+  return 1;
+}
